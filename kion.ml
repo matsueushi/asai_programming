a234954eff6eae7ea6f7f9f32b43601619e338df
matsueushi <matsueushi@gmail.com>
2018-12-23 16:50:04 -0500

    kaiteki

--- kion.ml
+++ kion.ml
@@ -1,8 +1,21 @@
+(* 目的 : 現在の気温 t が 15 以上 25以下かどうかをチェックする *)
+(* kaiteki : int -> bool *)
+let kaiteki t =
+    15 <= t && t <= 25
+
+(* テスト *)
+let test1 = kaiteki  7 = false
+let test2 = kaiteki 15 = true
+let test3 = kaiteki 20 = true
+let test4 = kaiteki 25 = true
+let test5 = kaiteki 28 = false
+
+
 (* 目的 : 現在の気温 t から快適度を表す文字列を計算する *)
 (* kion : int -> string *)
 let kion t = 
-    if 15 <= t && t <= 25 then "快適" 
-                          else "普通"
+    if kaiteki t then "快適" 
+                 else "普通"
 
 (* テスト *)
 let test1 = kion  7 = "普通"
