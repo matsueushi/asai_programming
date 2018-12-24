3a4b47947b9fadcd86d070a55e6d73b4bd4c4320
matsueushi <matsueushi@gmail.com>
2018-12-23 19:55:01 -0500

    Ex 5.6 kyosuukai

--- hanbetsusiki.ml
+++ hanbetsusiki.ml
@@ -17,8 +17,18 @@ let kai_no_kosuu a b c =
                                  else if hanbetsushiki a b c = 0.0 then 1
                                                                    else 2
 
-
 (* テスト *)
 let kai_no_kosuu_test1 = kai_no_kosuu   1.0   0.0   1.0   = 0
 let kai_no_kosuu_test2 = kai_no_kosuu   3.0   5.0   2.0   = 2
 let kai_no_kosuu_test3 = kai_no_kosuu (-1.0)  2.0 (-1.0)  = 1 
+
+
+(* 目的 : 2次方程式 ax^2 + bx + c = 0 の係数　a, b, c (いずれも実数とする) *)
+(*       を与えられたら、この2時方程式が虚数解を持つかどうかを判定する         *)
+(* kyosuukai : float -> float -> float -> bool                       *)
+let kyosuukai a b c = hanbetsushiki a b c < 0.0 
+
+(* テスト *)
+let kyosuukai_test1 = kyosuukai   1.0   0.0   1.0   = true 
+let kyosuukai_test2 = kyosuukai   3.0   5.0   2.0   = false
+let kyosuukai_test3 = kyosuukai (-1.0)  2.0 (-1.0)  = false 
