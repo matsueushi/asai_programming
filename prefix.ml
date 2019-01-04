6ae7255a69fef2e0a504d4f67d5b960ee3ce0ec3
matsueushi <matsueushi@gmail.com>
2019-01-03 22:58:19 -0500

    typo

--- prefix.ml
+++ prefix.ml
@@ -4,7 +4,7 @@ let rec add_to_each n lst = match lst with
     [] -> []
     | first :: rest -> (n :: first) :: add_to_each n rest
 
-(* 目的 : 受け取った lst の要素それぞれの先頭に n を付け加える *) (* テスト *)
+(* テスト *)
 let test1 = add_to_each 1 [] = []
 let test2 = add_to_each 1 [[2]] = [[1; 2]]
 let test3 = add_to_each 1 [[2]; [2; 3]] = [[1; 2]; [1; 2; 3]]
