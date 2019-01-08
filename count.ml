ae5470990fcbdba755ca30a3f2edd82fe0ad4d11
matsueushi <matsueushi@gmail.com>
2019-01-07 22:04:55 -0500

    Ex 14.6 count (filter & length version), Ex 14.4 (local function version)

--- count.ml
+++ count.ml
@@ -1,12 +1,25 @@
 #use "gakusei_t.ml"
 
+(* Section 13.1 version *)
+
 (* 目的 : 学生リスト lst のうち成績が seiseki0 の人の数を返す *)
 (* count : gakusei_t list -> string -> int *)
-let rec count lst seiseki0 = match lst with
+(* let rec count lst seiseki0 = match lst with
     [] -> 0
     | {name = n; score = s; grade = g} :: rest
         -> if g = seiseki0 then 1 + count rest seiseki0
-           else count rest seiseki0
+           else count rest seiseki0 *)
+
+
+(* Section 14.4 version *)
+
+(* 目的 : 学生リスト lst のうち成績が seiseki0 の人の数を返す *)
+(* count : gakusei_t list -> string -> int *)
+let rec count lst seiseki0 = 
+    (* 目的 : gakusei_t 型の gakusei の成績が seiseki0 かどうかを返す *)
+    let is_gakusei_seiseki0 gakusei = match gakusei with
+        {name = n; score = s; grade = g} -> g = seiseki0 in
+            List.length (List.filter is_gakusei_seiseki0 lst)
 
 (* テスト *)
 let test1 = count lst1 "A" = 0
