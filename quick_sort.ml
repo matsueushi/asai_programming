6019d41df3dde2c4f0acda77e170781a2af2a262
matsueushi <matsueushi@gmail.com>
2019-01-09 00:17:32 -0500

    Ex 15.1 quick_sort bug fix, Section 15.4 e

--- quick_sort.ml
+++ quick_sort.ml
@@ -9,10 +9,13 @@
     (* 目的 : lst の中から n より大きい要素のみを取り出す *)
     (* take_greater : int -> int list -> int list *)
     in let take_greater n lst = take n lst (>)
+    (* 目的 : lst の中から n と等しい要素のみを取り出す *)
+    (* take_equal : int -> int list -> int list *)
+    in let take_equal n lst = take n lst (=)
     in match lst with 
     [] -> [] (* 自明のケース *)
     | first :: rest -> quick_sort (take_less first rest)
-                       @ [first]
+                       @ take_equal first lst 
                        @ quick_sort (take_greater first rest)
 
 (* テスト *)
@@ -21,3 +24,5 @@ let test2 = quick_sort [1] = [1]
 let test3 = quick_sort [1; 2] = [1; 2]
 let test4 = quick_sort [2; 1] = [1; 2]
 let test5 = quick_sort [5; 4; 9; 8; 2; 3] = [2; 3; 4; 5; 8; 9]
+(* Ex 15.1 *)
+let test6 = quick_sort [1; 1] = [1; 1]
