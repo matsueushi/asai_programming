60f60ff53e7a591a7301f33d3b86fc91ab68b8c7
matsueushi <matsueushi@gmail.com>
2019-01-15 21:27:25 -0500

    Ex 17.14 get_ekikan_kyori, Ex 17.15 dijkstra_main.ml

--- dijkstra_main.ml
+++ dijkstra_main.ml
@@ -4,6 +4,8 @@
 (* eki_t.mlを二度読み込むのを防ぐため *)
 (* Ex 16.3 version *)
 
+(*
+
 #use "get_ekikan_kyori.ml"
 
 (* 目的 : 直前に確定した駅 p (eki_t 型) と未確定の駅のリスト lst (eki_t list 型)  *)
@@ -59,3 +61,64 @@ let result_eki_list = [
 ]
 
 let test = dijkstra_main sample_eki_list sample_ekikan_list = result_eki_list
+
+                                                                    *)
+
+(* Ex 17.14 version *)
+
+#use "get_ekikan_kyori.ml"
+
+(* 目的 : 直前に確定した駅 p (eki_t 型) と未確定の駅のリスト lst (eki_t list 型)  *)
+(*       を受け取ったら、必要な更新処理を行った後の未確定の駅のリストを返す         *)
+(* koushin : eki_t -> eki_t list -> ekikan_tree_t -> eki_t list *)
+let koushin p lst ekikan_tree = match p with
+    {namae = n_p; saitan_kyori = s_p; temae_list = t_p} ->
+    List.map 
+    (fun q -> match q with 
+    {namae = n_q; saitan_kyori = s_q; temae_list = t_q} ->
+    let ekikan = get_ekikan_kyori n_p n_q ekikan_tree in
+        if ekikan = infinity then q
+        else if s_p +. ekikan < s_q then
+            {namae = n_q; saitan_kyori = s_p +. ekikan; temae_list = n_q :: t_p}
+        else q)
+    lst
+
+(* 目的 : eki_t list 型の (未確定の) リストとekikan_t list 型の駅間のリスト *)
+(*       を受け取ったら、ダイクストラのアルゴリズムに従って各駅について最短距離  *)
+(*       と最短経路が正しく入ったリスト(eki_t list 型)を返す                *)
+(* dijkstra_main : eki_t list -> ekikan_t list -> eki_t list *)
+let rec dijkstra_main eki_lst ekikan_lst = match eki_lst with
+    [] -> []
+    | first :: rest -> 
+        let (saitan, nokori) = saitan_wo_bunri eki_lst in
+        let updated_nokori = koushin saitan nokori ekikan_lst in
+        saitan :: dijkstra_main updated_nokori ekikan_lst
+
+(* length of nokori = length of eki_lst - 1 より終了する *)
+
+(* テスト *)
+let sample_eki_list = [
+    {namae = "A"; saitan_kyori = 0.0; temae_list = ["A"]};
+    {namae = "B"; saitan_kyori = infinity; temae_list = []};
+    {namae = "C"; saitan_kyori = infinity; temae_list = []};
+    {namae = "D"; saitan_kyori = infinity; temae_list = []};
+    {namae = "E"; saitan_kyori = infinity; temae_list = []}
+]
+let sample_ekikan_list = [
+    {kiten = "A"; shuten = "D"; keiyu = ""; kyori = 4.0; jikan = 0};
+    {kiten = "A"; shuten = "B"; keiyu = ""; kyori = 10.0; jikan = 0};
+    {kiten = "D"; shuten = "E"; keiyu = ""; kyori = 3.0; jikan = 0};
+    {kiten = "B"; shuten = "E"; keiyu = ""; kyori = 2.0; jikan = 0};
+    {kiten = "B"; shuten = "C"; keiyu = ""; kyori = 2.0; jikan = 0};
+    {kiten = "E"; shuten = "C"; keiyu = ""; kyori = 1.0; jikan = 0};
+]
+let result_eki_list = [
+    {namae = "A"; saitan_kyori = 0.0; temae_list = ["A"]};
+    {namae = "D"; saitan_kyori = 4.0; temae_list = ["D"; "A"]};
+    {namae = "E"; saitan_kyori = 7.0; temae_list = ["E"; "D"; "A"]};
+    {namae = "C"; saitan_kyori = 8.0; temae_list = ["C"; "E"; "D"; "A"]};
+    {namae = "B"; saitan_kyori = 9.0; temae_list = ["B"; "E"; "D"; "A"]}
+]
+
+let sample_ekikan_tree_t = inserts_ekikan Empty sample_ekikan_list
+let test = dijkstra_main sample_eki_list sample_ekikan_tree_t = result_eki_list
--- get_ekikan_kyori.ml
+++ get_ekikan_kyori.ml
@@ -1,3 +1,5 @@
+(* Ex 10.11 version 
+
 #use "ekikan_t.ml"
 
 (* 目的 : 始点と終点の駅名と(文字列)と駅間リスト(ekikan_t list型)を受け取ったら、 *)
@@ -21,7 +23,7 @@ let get_route_kyori_test4 =
     get_route_kyori "広尾" "六本木" global_ekikan_list = 1.7
 
 
-(* 目的 : 感じの駅名を二つ(文字列)と駅間リスト(ekikan_t list型)を受け取ったら、 *)
+(* 目的 : 漢字の駅名を二つ(文字列)と駅間リスト(ekikan_t list型)を受け取ったら、 *)
 (*       駅間リストの中から2駅間の距離を返す                                *)
 (*       どちらの順番で問い合わせても正しく距離が帰ってくるように作る           *)
 (*       直接繋がっていないときは infinity が返ってくる                     *)
@@ -38,3 +40,30 @@ let get_ekikan_kyori_test3 =
     get_ekikan_kyori "新大塚" "茗荷谷" global_ekikan_list = 1.2
 let get_ekikan_kyori_test4 = 
     get_ekikan_kyori "広尾" "六本木" global_ekikan_list = 1.7
+
+                                                            *)
+
+(* Ex 17.14 version *)
+#use "assoc.ml"
+#use "ekikan_t.ml"
+#use "inserts_ekikan.ml"
+
+(* 目的 :  漢字の駅名二つと ekikan_tree_t 型の気を受け取り、2 駅間の距離を返す *)
+(* get_ekikan_kyori : string -> string -> ekikan_tree_t -> float *)
+let rec get_ekikan_kyori eki1 eki2 tree = match tree with 
+    Empty -> infinity
+    | Node (t1, eki, lst, t2) -> 
+        if eki1 = eki then assoc eki2 lst  
+        else if eki1 < eki then get_ekikan_kyori eki1 eki2 t1
+        else get_ekikan_kyori eki1 eki2 t2 
+        
+(* テスト *)
+let global_ekikan_tree_t = inserts_ekikan Empty global_ekikan_list
+let get_ekikan_kyori_test1 = 
+    get_ekikan_kyori "茗荷谷" "新大塚" global_ekikan_tree_t = 1.2
+let get_ekikan_kyori_test2 = 
+    get_ekikan_kyori "渋谷" "日本橋" global_ekikan_tree_t = infinity
+let get_ekikan_kyori_test3 = 
+    get_ekikan_kyori "新大塚" "茗荷谷" global_ekikan_tree_t = 1.2
+let get_ekikan_kyori_test4 = 
+    get_ekikan_kyori "広尾" "六本木" global_ekikan_tree_t = 1.7
