036ca772c1e4f50a6fdc40580447177b6274fb39
matsueushi <matsueushi@gmail.com>
2019-01-19 10:28:04 -0500

    Section 18.5 price, total_price

--- price.ml
+++ price.ml
@@ -1,12 +1,32 @@
+(* Section 18.1 version *)
+
 (* 目的 : item の値段を調べる *)
 (* price : string -> (string * int) list -> int option *)
-let rec price item yaoya_list = match yaoya_list with
+(* let rec price item yaoya_list = match yaoya_list with
     [] -> None
     | (yasai, nedan) :: rest ->
         if item = yasai then Some (nedan)
-        else price item rest
+        else price item rest *)
 
 (* テスト *)
-let yaoya_list = [("トマト", 300); ("たまねぎ", 200); ("にんじん", 150); ("ほうれん草", 200)]
+(* let yaoya_list = [("トマト", 300); ("たまねぎ", 200); ("にんじん", 150); ("ほうれん草", 200)]
 let test1 = price "トマト" yaoya_list = Some (300)
-let test2 = price "じゃがいも" yaoya_list = None
+let test2 = price "じゃがいも" yaoya_list = None *)
+
+(* Section 18.5 version *)
+
+(* 売り切れを示す例外 *)
+exception Urikire
+
+(* 目的 : item の値段を調べる *)
+(* 見つからないときは Urikire という例外を発生する *)
+(* price : string -> (string * int) list -> option *)
+let rec price item yaoya_list = match yaoya_list with
+    [] -> raise Urikire 
+    | (yasai, nedan) :: rest ->
+        if item = yasai then nedan
+        else price item rest
+        
+(* テスト *)
+let yaoya_list = [("トマト", 300); ("たまねぎ", 200); ("にんじん", 150); ("ほうれん草", 200)]
+let test1 = price "トマト" yaoya_list = 300
--- total_price.ml
+++ total_price.ml
@@ -1,14 +1,59 @@
 #use "price.ml"
 
+(* Section 18.2 version *)
+
 (* 目的 : yasai_list を買った時の値段の合計を調べる *)
 (* total_price : string list -> (string * int) list -> int *)
-let rec total_price yasai_list yaoya_list = match yasai_list with
+(* let rec total_price yasai_list yaoya_list = match yasai_list with
     [] -> 0
     | first :: rest ->
         match price first yaoya_list with
             None -> total_price rest yaoya_list
-            | Some (p) -> p + total_price rest yaoya_list
+            | Some (p) -> p + total_price rest yaoya_list *)
 
-(* 目的 *)
+(* テスト *)
+(* let test1 = total_price ["トマト"; "たまねぎ"] yaoya_list = 500
+let test2 = total_price ["トマト"; "じゃがいも"] yaoya_list = 300  *)
+
+
+(* Section 18.3 version *)
+
+(* 目的 : yasai_list を買った時の値段の合計を調べる *)
+(* total_price : string list -> (string * int) list -> int *)
+(* let rec total_price yasai_list yaoya_list =
+    (* 目的 : yasai_list を買った時の値段の合計を調べる補助関数 *)
+    (* hojo : string list -> int option *)
+    let rec hojo yasai_list = match yasai_list with
+    [] -> Some(0)
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> None
+            | Some (p) -> match hojo rest with
+                            None -> None
+                            | Some (q) -> Some(p + q)
+    in match hojo yasai_list with
+        None -> 0
+        | Some (p) -> p  *)
+
+
+
+(* Section 18.5 version *)
+
+(* 目的 : yasai_list を買った時の値段の合計を調べる *)
+(* total_price : string list -> (string * int) list -> int *)
+let rec total_price yasai_list yaoya_list =
+    (* 目的 : yasai_list を買った時の値段の合計を調べる補助関数 *)
+    (* hojo : string list -> int *)
+    let rec hojo yasai_list = match yasai_list with
+    [] -> 0
+    | first :: rest ->
+        price first yaoya_list + hojo rest
+    in try
+        hojo yasai_list 
+        with Urikire -> 0
+
+(* テスト *)
 let test1 = total_price ["トマト"; "たまねぎ"] yaoya_list = 500
-let test2 = total_price ["トマト"; "じゃがいも"] yaoya_list = 300 
+let test2 = total_price ["トマト"; "じゃがいも"] yaoya_list = 0 
+let test3 = total_price ["たまねぎ"; "にんじん"] yaoya_list = 350
+let test4 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list = 0
